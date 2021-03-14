7ff2069cceb771088971f19d19e78c626072e03c
kaiweinan <kaiweinan@gmail.com>
2021-03-14 09:47:18 -0400

    commit tree d9c89bc026038ebe0545c1eaad01af387fca04b3

--- app.rb
+++ app.rb
@@ -2,7 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+	get "/info" do
+		erb :info
+	  end 
+
 	get '/' do
-		
+		erb :index
+	  end 
+
+	get '/' do
+		"<h1>Hello World</h1>"
 	end
+
+	
+	get "/info" do
+		"Testing the info page"
+	  end 
+
+	  
 end
